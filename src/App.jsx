bbc52a0bad7a859ae1edac0ba898d9f36f4b31df
Eduardo <educonts08@gmail.com>
2022-10-14 00:03:57 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ import { Projects } from './components/Projects'
 function App() {
 
   return (
-    <div>
-    <div id="portfolio">
-      <h1>Carlos Eduardo</h1>
-      <Sidebar />
-      <MainContent />
-    </div>
+    <>
+      <div id="portfolio">
+        <h1>Carlos Eduardo</h1>
+        <Sidebar />
+        <MainContent />
+      </div>
       <div className="project-content">
         <Projects />
       </div>
-    </div>
+    </>
   )
 }
 
